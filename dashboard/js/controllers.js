9e533658b20a1bb681b3a6ee13944c22d5e33f56
Ashish Dubey <ashish.dubey91@gmail.com>
2014-03-04 02:58:34 +0530

    ngMagic, remove directives and use data-bindings

--- dashboard/js/controllers.js
+++ dashboard/js/controllers.js
@@ -17,6 +17,31 @@ dashboardControllers.controller('profile',
 dashboardControllers.controller('websites',
     ['$scope', 'websites',
     function ($scope, websites) {
+        $scope.newSite = {};
+
+        $scope.addSite = function() {
+            websites.save($scope.newSite);
+            $scope.sites.push($scope.newSite);
+            $scope.newSite = {
+                name: '',
+                url: ''
+            };
+        };
+
+        $scope.tmpSite = {};
+
+        $scope.editSiteOpen = function(site) {
+            site.updated = {
+                'name': site.name,
+                'url': site.url
+            };
+        };
+
+        $scope.updateSite = function(site) {
+            websites.update({websiteId: site.id}, site.updated);
+            site.name = site.updated.name;
+            site.url = site.updated.url;
+        };
 
         $scope.sites = websites.query();
     }

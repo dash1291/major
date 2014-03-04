1da37ec6b8887e40617c251ff885c1ef801105ac
Ashish Dubey <ashish.dubey91@gmail.com>
2014-03-04 13:31:09 +0530

    remove unused getCookie()

--- dashboard/js/services.js
+++ dashboard/js/services.js
@@ -7,22 +7,6 @@ conceptualServices.config(['$httpProvider', function($http) {
     $http.defaults.xsrfHeaderName = 'X-CSRFToken';
 }]);
 
-function getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = cookies[i].trim();
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
-
 conceptualServices.factory('profile',
     ['$resource',
     function($resource) {

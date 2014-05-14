0d54a784c2f1a626948faf7d51afa111dced5434
Ashish Dubey <ashish.dubey91@gmail.com>
2014-05-14 10:54:01 +0530

    UI fixups

--- dashboard/js/controllers.js
+++ dashboard/js/controllers.js
@@ -50,8 +50,10 @@ dashboardControllers.controller('websites',
         };
 
         $scope.deleteSite = function() {
-            Website.delete({websiteId: $scope.deleted.id});
-            $scope.sites.pop($scope.deleted);
+            Website.delete({websiteId: $scope.deleted.id}, function() {
+                $scope.sites.splice($scope.sites.indexOf($scope.deleted));
+            });
+
         };
 
         $scope.sites = Website.query();
@@ -93,7 +95,6 @@ dashboardControllers.controller('pages',
 
         $scope.editPageOpen = function(page) {
             updatedPage = page;
-            console.log(updatedPage);
             $scope.updated = {
                 url: page.url,
                 name: page.name
@@ -101,9 +102,12 @@ dashboardControllers.controller('pages',
         };
 
         $scope.updatePage = function() {
-            Page.update({websiteId: selectedSiteId, pageId: updatedPage.id}, $scope.updated);
-            updatedPage.name = $scope.updated.name;
-            updatedPage.url = $scope.updated.url;
+            Page.update({websiteId: selectedSiteId, pageId: updatedPage.id},
+                $scope.updated, function() {
+                    updatedPage.name = $scope.updated.name;
+                    updatedPage.url = $scope.updated.url;
+                }
+            );
         };
 
         $scope.deletePageOpen = function(page) {
@@ -111,8 +115,12 @@ dashboardControllers.controller('pages',
         };
 
         $scope.deletePage = function() {
-            Page.delete({websiteId: selectedSiteId, pageId: $scope.deleted.id});
-            $scope.pages.pop($scope.deleted);
+            Page.delete({websiteId: selectedSiteId, pageId: $scope.deleted.id},
+                function() {
+                    $scope.pages.splice($scope.pages.indexOf($scope.deleted));
+
+                }
+            );
         };
     }
 ]);
